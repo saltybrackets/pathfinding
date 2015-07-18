67f8b169405bad26077f9cb73228874939e42b77
Zachary White <zallenwhite@gmail.com>
2015-07-18 00:09:58 -0700

    Additional unit testing. Third maze failing (not the same bit depth).

--- PathFindingTests/Map/MazeTests.cs
+++ PathFindingTests/Map/MazeTests.cs
@@ -11,13 +11,52 @@ namespace PathFindingTests
 	[TestClass]
 	public class MazeTests
 	{
+
 		[TestMethod]
-		public void ParseMazeTest()
+		public void ParseMaze1Test()
 		{
-			System.Console.WriteLine("Reading maze bitmap.");
-			Maze maze = new Maze(PathFindingTests.Properties.Resources.maze2);
-			System.Console.WriteLine(maze);
-			Assert.AreEqual(true, true);
+			System.Console.WriteLine("Analyzing maze1.");
+			Maze maze = new Maze(PathFindingTests.Properties.Resources.maze1);
+			CheckMap(maze);
 		}
+
+
+		[TestMethod]
+		public void ParseMaze2Test()
+		{
+			System.Console.WriteLine("Analyzing maze2.");
+			Maze maze = new Maze(PathFindingTests.Properties.Resources.maze2);
+			CheckMap(maze);
+		}
+
+
+		[TestMethod]
+		public void ParseMaze3Test()
+		{
+			System.Console.WriteLine("Analyzing maze3.");
+			Maze maze = new Maze(PathFindingTests.Properties.Resources.maze3);
+			CheckMap(maze);
+		}
+
+
+		private void CheckMap(Maze maze)
+		{
+			System.Console.WriteLine(maze);
+			System.Console.WriteLine();
+
+			int bitmapPixels = maze.Bitmap.Width * maze.Bitmap.Height;
+			int emptyPixels = maze.GetEmptyPixelsAmount();
+			int endPixels = maze.GetEndPixelsAmount();
+			int startPixels = maze.GetStartPixelsAmount();
+			int wallPixels = maze.GetWallPixelsAmount();
+			int totalFeatures = emptyPixels + endPixels + startPixels + wallPixels;
+
+			Assert.AreNotEqual(emptyPixels, 0);
+			Assert.AreNotEqual(endPixels, 0);
+			Assert.AreNotEqual(startPixels, 0);
+			Assert.AreNotEqual(wallPixels, 0);
+			Assert.AreEqual(totalFeatures, bitmapPixels);
+		}
+
 	}
 }
